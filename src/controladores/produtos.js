6d50f3a1d43600154eedd82f3401e6f29e93745a
Natanael PN <nathanaelpassos@gmail.com>
2024-01-07 14:14:17 -0300

    Criado e otimizado função para upload de arquivos com imagem

--- src/controladores/produtos.js
+++ src/controladores/produtos.js
@@ -1,158 +1,175 @@
-const knex = require('../conexao');
+const knex = require("../conexao");
+const { uploadImagem } = require("../servicos/uploads");
 
 const listarProdutos = async (req, res) => {
-    const { usuario } = req;
-    const { categoria } = req.query;
+  const { usuario } = req;
+  const { categoria } = req.query;
 
-    try {
-        const produtos = await knex('produtos')
-            .where({ usuario_id: usuario.id })
-            .where(query => {
-                if (categoria) {
-                    return query.where('categoria', 'ilike', `%${categoria}%`);
-                }
-            });
+  try {
+    const produtos = await knex("produtos")
+      .where({ usuario_id: usuario.id })
+      .where((query) => {
+        if (categoria) {
+          return query.where("categoria", "ilike", `%${categoria}%`);
+        }
+      });
 
-        return res.status(200).json(produtos);
-    } catch (error) {
-        return res.status(400).json(error.message);
-    }
-}
+    return res.status(200).json(produtos);
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
 
 const obterProduto = async (req, res) => {
-    const { usuario } = req;
-    const { id } = req.params;
+  const { usuario } = req;
+  const { id } = req.params;
 
-    try {
-        const produto = await knex('produtos').where({
-            id,
-            usuario_id: usuario.id
-        }).first();
+  try {
+    const produto = await knex("produtos")
+      .where({
+        id,
+        usuario_id: usuario.id,
+      })
+      .first();
 
-        if (!produto) {
-            return res.status(404).json('Produto não encontrado');
-        }
-
-        return res.status(200).json(produto);
-    } catch (error) {
-        return res.status(400).json(error.message);
+    if (!produto) {
+      return res.status(404).json("Produto não encontrado");
     }
-}
+
+    return res.status(200).json(produto);
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
 
 const cadastrarProduto = async (req, res) => {
-    const { usuario } = req;
-    const { nome, estoque, preco, categoria, descricao, imagem } = req.body;
+  const { usuario } = req;
+  const { nome, estoque, preco, categoria, descricao } = req.body;
 
-    if (!nome) {
-        return res.status(404).json('O campo nome é obrigatório');
+  const { originalname, buffer, mimetype } = req.file;
+
+  if (!descricao) {
+    return res.status(404).json("O campo descricao é obrigatório");
+  }
+
+  try {
+    let produto = await knex("produtos")
+      .insert({
+        usuario_id: usuario.id,
+        nome,
+        estoque,
+        preco,
+        categoria,
+        descricao,
+      })
+      .returning("*");
+
+    if (!produto) {
+      return res.status(400).json("O produto não foi cadastrado");
     }
 
-    if (!estoque) {
-        return res.status(404).json('O campo estoque é obrigatório');
-    }
+    const id = produto[0].id;
 
-    if (!preco) {
-        return res.status(404).json('O campo preco é obrigatório');
-    }
+    const imagem = await uploadImagem(
+      `produtos/${id}/${originalname}`,
+      buffer,
+      mimetype
+    );
 
-    if (!descricao) {
-        return res.status(404).json('O campo descricao é obrigatório');
-    }
+    produto = await knex("produtos")
+      .update({
+        imagem: imagem.path,
+      })
+      .where({ id })
+      .returning("*");
 
-    try {
-        const produto = await knex('produtos').insert({
-            usuario_id: usuario.id,
-            nome,
-            estoque,
-            preco,
-            categoria,
-            descricao,
-            imagem
-        }).returning('*');
+    produto[0].urlImagem = imagem.url;
 
-        if (!produto) {
-            return res.status(400).json('O produto não foi cadastrado');
-        }
-
-        return res.status(200).json(produto);
-    } catch (error) {
-        return res.status(400).json(error.message);
-    }
-}
+    return res.status(200).json(produto[0]);
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
 
 const atualizarProduto = async (req, res) => {
-    const { usuario } = req;
-    const { id } = req.params;
-    const { nome, estoque, preco, categoria, descricao, imagem } = req.body;
+  const { usuario } = req;
+  const { id } = req.params;
+  const { nome, estoque, preco, categoria, descricao, imagem } = req.body;
 
-    if (!nome && !estoque && !preco && !categoria && !descricao && !imagem) {
-        return res.status(404).json('Informe ao menos um campo para atualizaçao do produto');
+  if (!nome && !estoque && !preco && !categoria && !descricao && !imagem) {
+    return res
+      .status(404)
+      .json("Informe ao menos um campo para atualizaçao do produto");
+  }
+
+  try {
+    const produtoEncontrado = await knex("produtos")
+      .where({
+        id,
+        usuario_id: usuario.id,
+      })
+      .first();
+
+    if (!produtoEncontrado) {
+      return res.status(404).json("Produto não encontrado");
     }
 
-    try {
-        const produtoEncontrado = await knex('produtos').where({
-            id,
-            usuario_id: usuario.id
-        }).first();
+    const produto = await knex("produtos").where({ id }).update({
+      nome,
+      estoque,
+      preco,
+      categoria,
+      descricao,
+      imagem,
+    });
 
-        if (!produtoEncontrado) {
-            return res.status(404).json('Produto não encontrado');
-        }
-
-        const produto = await knex('produtos')
-            .where({ id })
-            .update({
-                nome,
-                estoque,
-                preco,
-                categoria,
-                descricao,
-                imagem
-            });
-
-        if (!produto) {
-            return res.status(400).json("O produto não foi atualizado");
-        }
-
-        return res.status(200).json('produto foi atualizado com sucesso.');
-    } catch (error) {
-        return res.status(400).json(error.message);
+    if (!produto) {
+      return res.status(400).json("O produto não foi atualizado");
     }
-}
+
+    return res.status(200).json("produto foi atualizado com sucesso.");
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
 
 const excluirProduto = async (req, res) => {
-    const { usuario } = req;
-    const { id } = req.params;
+  const { usuario } = req;
+  const { id } = req.params;
 
-    try {
-        const produtoEncontrado = await knex('produtos').where({
-            id,
-            usuario_id: usuario.id
-        }).first();
+  try {
+    const produtoEncontrado = await knex("produtos")
+      .where({
+        id,
+        usuario_id: usuario.id,
+      })
+      .first();
 
-        if (!produtoEncontrado) {
-            return res.status(404).json('Produto não encontrado');
-        }
-
-        const produtoExcluido = await knex('produtos').where({
-            id,
-            usuario_id: usuario.id
-        }).del();
-
-        if (!produtoExcluido) {
-            return res.status(400).json("O produto não foi excluido");
-        }
-
-        return res.status(200).json('Produto excluido com sucesso');
-    } catch (error) {
-        return res.status(400).json(error.message);
+    if (!produtoEncontrado) {
+      return res.status(404).json("Produto não encontrado");
     }
-}
+
+    const produtoExcluido = await knex("produtos")
+      .where({
+        id,
+        usuario_id: usuario.id,
+      })
+      .del();
+
+    if (!produtoExcluido) {
+      return res.status(400).json("O produto não foi excluido");
+    }
+
+    return res.status(200).json("Produto excluido com sucesso");
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
 
 module.exports = {
-    listarProdutos,
-    obterProduto,
-    cadastrarProduto,
-    atualizarProduto,
-    excluirProduto
-}
+  listarProdutos,
+  obterProduto,
+  cadastrarProduto,
+  atualizarProduto,
+  excluirProduto,
+};
--- src/rotas.js
+++ src/rotas.js
@@ -1,29 +1,30 @@
-const express = require('express');
-const usuarios = require('./controladores/usuarios');
-const login = require('./controladores/login');
-const produtos = require('./controladores/produtos');
-const verificaLogin = require('./filtros/verificaLogin');
+const express = require("express");
+const usuarios = require("./controladores/usuarios");
+const login = require("./controladores/login");
+const produtos = require("./controladores/produtos");
+const verificaLogin = require("./filtros/verificaLogin");
+const multer = require("./filtros/multer");
 
 const rotas = express();
 
 // cadastro de usuario
-rotas.post('/usuarios', usuarios.cadastrarUsuario);
+rotas.post("/usuarios", usuarios.cadastrarUsuario);
 
 // login
-rotas.post('/login', login.login);
+rotas.post("/login", login.login);
 
 // filtro para verificar usuario logado
 rotas.use(verificaLogin);
 
 // obter e atualizar perfil do usuario logado
-rotas.get('/perfil', usuarios.obterPerfil);
-rotas.put('/perfil', usuarios.atualizarPerfil);
+rotas.get("/perfil", usuarios.obterPerfil);
+rotas.put("/perfil", usuarios.atualizarPerfil);
 
 // crud de produtos
-rotas.get('/produtos', produtos.listarProdutos);
-rotas.get('/produtos/:id', produtos.obterProduto);
-rotas.post('/produtos', produtos.cadastrarProduto);
-rotas.put('/produtos/:id', produtos.atualizarProduto);
-rotas.delete('/produtos/:id', produtos.excluirProduto);
+rotas.get("/produtos", produtos.listarProdutos);
+rotas.get("/produtos/:id", produtos.obterProduto);
+rotas.post("/produtos", multer.single("imagem"), produtos.cadastrarProduto);
+rotas.put("/produtos/:id", produtos.atualizarProduto);
+rotas.delete("/produtos/:id", produtos.excluirProduto);
 
 module.exports = rotas;
